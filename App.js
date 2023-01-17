82fe915fa7a70f60d6a3acbbc04b5dd2c08e3696
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-01-17 11:50:10 -0800

    work through fundamentals

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import Cat from './Cat';
+import HelloWorldApp from './HelloWorldApp';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
+    {/* <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Let's go! First time using React Native Text syntax</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
+    <HelloWorldApp></HelloWorldApp> */}
+    <ScrollView>
+      <Text>Some Text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{width: 200, height: 200}}
+        />
+      </View>
+      <Cat/>
+      <TextInput 
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue='Type in here'
+      />
+    </ScrollView>
+
+    </>
   );
 }
 
---
+++ Cat.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Cat = () => {
+  const name = 'Maru'
+  return (
+    <Text>Hello, I"m a cat named {name}</Text>
+  )
+}
+
+export default Cat
